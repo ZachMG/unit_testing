4c9b47052c2c66671230f33ea84459e02b3b2f06
ZachMG <zmg1@alfred.edu>
2017-03-13 14:38:37 -0400

    Update Unit_Testing2.py
    edited to make unit tests slightly more efficient

--- Unit_Testing2.py
+++ Unit_Testing2.py
@@ -5,24 +5,23 @@ class UnitTests(unittest.TestCase):
     def setUp(self):
         print('setUp()...')
         self.hash1 = Hash('1234')
-        self.hash2 = Hash('1234')
-        self.hash3 = Hash('123')
-        self.email1 = Email('P@V')
+        self.email1 = Email('zmg@verizon.net')
 
     def test(self):
         print('testing hash...')
-        self.assertEqual(self.hash1, self.hash2) #failed
-        self.assertNotEqual(self.hash1, self.hash3)
-        self.assertRaises(InvalidPassword, Hash, '1')
+        self.assertEqual(self.hash1, self.hash1) #failed
+        self.assertNotEqual(self.hash1, Hash('123'))
+        self.assertRaises(InvalidPassword, Hash, '1 ') #failed
+        #self.assertEqual(length of Hash for two different passwords)
         print('testing email...')
-        self.assertEqual(str(self.email1), 'P@V')
-        self.assertRaises(InvalidEmail, Email, 'thing')
+        self.assertEqual(str(self.email1), 'zmg@verizon.net')
+        self.assertRaises(InvalidEmail, Email, '@@') #failed
         self.assertRaises(InvalidEmail, Email, '@gmail.com') #failed
         print('testing social...')
         self.assertRaises(InvalidSocial, SS, '123456789')
         self.assertRaises(InvalidSocial, SS, '1234-567-89') #failed
         self.assertRaises(InvalidSocial, SS, '-') #failed
-        self.assertRaises(InvalidSocial, SS, '1234')
+        self.assertRaises(InvalidSocial, SS, '1234-') #failed
 
     def tearDown(self):
         print('tearDown()...')
